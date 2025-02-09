a17e59559e91375580eebbc0b795c49bbc832e0e
Fabio Almeida Lima <fabio.allima@hotmail.com>
2025-02-09 11:44:14 -0300

    Refactoring Spring Batch

---
+++ src/main/java/com/example/SpringBatch/batch/tasklet/PrintHelloTasklet.java
@@ -0,0 +1,26 @@
+package com.example.SpringBatch.batch.tasklet;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Scanner;
+
+@Component
+@StepScope
+public class PrintHelloTasklet implements Tasklet {
+
+    @Value("${name}")
+    private String name;
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        System.out.println("Hello, " + name + "!");
+
+        return RepeatStatus.FINISHED;
+    }
+}
--- src/main/java/com/example/SpringBatch/config/BatchConfig.java
+++ src/main/java/com/example/SpringBatch/config/BatchConfig.java
@@ -3,30 +3,19 @@ package com.example.SpringBatch.config;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class BatchConfig {
 
     @Bean
-    public Job job(JobRepository jobRepository, Step step1) {
+    public Job job(JobRepository jobRepository, Step printHelloStep) {
         return new JobBuilder("job", jobRepository)
-                .start(step1)
-                .build();
-    }
-
-    @Bean
-    public Step step1(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-        return new StepBuilder("step1", jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("Hello, Spring Batch!");
-                    return RepeatStatus.FINISHED;
-                }, transactionManager)
+                .start(printHelloStep)
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 }
